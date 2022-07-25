981f576bfc1fa61c23384001f3a513308c36374d
ve001 <pcanimated@gmail.com>
2022-07-25 18:21:49 +0200

    HU2

---
+++ home/script.js
@@ -0,0 +1,97 @@
+// CopyWriter PopUp
+// ==========================================================
+
+function popupCopywriter() {
+    let popupCW = document.getElementById("popupCopywriter");
+
+    popupCW.style.display = "block";
+}
+function closeCWP() {
+    let popupCW = document.getElementById("popupCopywriter");
+
+    popupCW.style.display = "none";
+}
+
+// ==========================================================
+
+// Crypto PopUp
+// ==========================================================
+
+function opencrytotab() {
+    let popupCT = document.getElementById("popupCrypto");
+
+    popupCT.style.display = "block";
+}
+function closeCPP() {
+    let popupCT = document.getElementById("popupCrypto");
+
+    popupCT.style.display = "none";
+}
+
+// ==========================================================
+
+// Personal Finance PopUp
+// ==========================================================
+
+function PFpopup() {
+    let popupPF = document.getElementById("popupPF");
+
+    popupPF.style.display = "block";
+}
+function closePFPP() {
+    let popupPF = document.getElementById("popupPF");
+
+    popupPF.style.display = "none";
+}
+
+// ==========================================================
+
+// Amazon FBA PopUp
+// ==========================================================
+
+function openafba() {
+    let popupAF = document.getElementById("popupafba");
+
+    popupAF.style.display = "block";
+}
+function closeAFBA() {
+    let popupAF = document.getElementById("popupafba");
+
+    popupAF.style.display = "none";
+}
+
+// ==========================================================
+
+// Stocks PopUp
+// ==========================================================
+
+function showstockspopup() {
+    let popupS = document.getElementById("popupstocks");
+
+    popupS.style.display = "block";
+}
+function closeStocks() {
+    let popupS = document.getElementById("popupstocks");
+
+    popupS.style.display = "none";
+}
+
+// ==========================================================
+
+// Freelancing PopUp
+// ==========================================================
+
+function freelanceopup() {
+    let popupF = document.getElementById("popupFL");
+
+    popupF.style.display = "block";
+}
+function closeFL() {
+    let popupF = document.getElementById("popupFL");
+
+    popupF.style.display = "none";
+}
+
+// ==========================================================
+
+
---
+++ index/script.js
@@ -0,0 +1,10 @@
+function submit() {
+    let code = document.getElementById("codeInput").value;
+    console.log(code);
+
+    if (code == 2712183040151624) {
+        window.location.replace("6Wow2A3HPmnicbmDQtOn91fBVB8c8E.html");
+    } else {
+        alert("Wrong Code");
+    }
+}
